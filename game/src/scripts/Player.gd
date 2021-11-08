22977fd0c16181288ebce832a9c2564f2cd6d836
Agustin <agusgabrielli9@gmail.com>
2021-11-08 18:05:57 -0300

    salto parcial implementado

--- game/src/scripts/Player.gd
+++ game/src/scripts/Player.gd
@@ -1,17 +1,18 @@
 extends KinematicBody2D
 class_name Player
 
-const FLOOR_NORMAL: = Vector2.UP
-#export var horizontal_speed = 600.0
 export var speed: = Vector2(300.0, 1000.0)
 export var gravity = 3500.0
 export var jump_constant = 1000.0
-var velocity = Vector2.ZERO
-
 export var score: int = 0
 
+const FLOOR_NORMAL: = Vector2.UP
+var velocity = Vector2.ZERO
+
 func _physics_process(delta: float) -> void:
-	velocity = calculate_velocity(delta, velocity)
+	var is_jump_interrupted: bool = Input.is_action_just_released("jump") and velocity.y < 0.0
+
+	velocity = calculate_velocity(delta, velocity, is_jump_interrupted)
 	velocity = move_and_slide(velocity, FLOOR_NORMAL)
 
 	if velocity.x == 0:
@@ -20,22 +21,19 @@ func _physics_process(delta: float) -> void:
 func _get_direction_y() -> float:
 	return -1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
 
-func calculate_velocity(delta: float, previous_velocity: Vector2) -> Vector2:
+func calculate_velocity(delta: float, previous_velocity: Vector2, is_jump_interrupted: bool) -> Vector2:
 	var new_velocity: = previous_velocity
 	new_velocity.x += (speed.x * delta)
 	new_velocity.y += (gravity * delta)
-	print(new_velocity)
 	
 	var direction_y: = _get_direction_y()
 	if direction_y == -1.0:
 		new_velocity.y = speed.y * direction_y
-	
-	#if Input.is_action_just_pressed("jump"):
-		#print(get_position())
-	#	y -= jump_constant
-	
-	new_velocity.x = min(new_velocity.x, speed.x)
+	if is_jump_interrupted:
+		new_velocity.y = 0.0
 		
+	new_velocity.x = min(new_velocity.x, speed.x)
+	
 	return new_velocity
 
 # Función para incrementar el score del jugador.
