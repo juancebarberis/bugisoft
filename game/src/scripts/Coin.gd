78907ff4671f17e8a6171591a7deef3f44769dc1
Juan Barberis <juancebarberis@gmail.com>
2021-11-10 11:34:21 -0300

    added coin sounds

--- game/src/scripts/Coin.gd
+++ game/src/scripts/Coin.gd
@@ -1,11 +1,27 @@
 extends Area2D
 
+class_name Coin
+
 onready var animation = get_node("AnimationPlayer")
+onready var hit_sound = get_node("HitSound")
+onready var hit_sound_audio = preload("res://src/audio/coin_2.wav")
+
+var hit_sound_played = false
 
 # This function detects the collision with the Player
 func _on_body_entered(player: KinematicBody2D) -> void:
 	# Play the animation for the coin
 	animation.play("coin_destroy")
 	
+	# Play the sound
+	if not hit_sound_played:
+		_play_hit_sound()
+	
 	# Sumar un punto al contador
 	player.increase_score()
+	
+func _play_hit_sound():
+	hit_sound.stream = hit_sound_audio
+	hit_sound.volume_db = -2.0
+	hit_sound.play()
+	hit_sound_played = true
